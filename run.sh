e1c583b1bb5632e46f7381122f575ce315081b06
Sean Meadows <designed27@gmail.com>
2014-08-24 11:12:34 -0400

    More debug for src path

--- run.sh
+++ run.sh
@@ -7,20 +7,18 @@ fi
 
 if [ -f "$WERCKER_CACHE_DIR/google_appengine_$WERCKER_APPENGINE_DEPLOY_PATH_LATEST_SDKVERSION.zip" ];
 then
-    debug "Appengine SDK already installed."
+    debug "Appengine SDK $WERCKER_APPENGINE_DEPLOY_PATH_LATEST_SDKVERSION already installed."
     export PATH="$PATH:$WERCKER_CACHE_DIR/google_appengine"
 else
     sudo apt-get update
     sudo apt-get install unzip
     
     cd $WERCKER_CACHE_DIR
-    debug 'Downloading AppEngine SDK...'
+    debug 'Downloading AppEngine SDK $WERCKER_APPENGINE_DEPLOY_PATH_LATEST_SDKVERSION...'
     
     curl -O https://storage.googleapis.com/appengine-sdks/featured/google_appengine_$WERCKER_APPENGINE_DEPLOY_PATH_LATEST_SDKVERSION.zip ; unzip -x google_appengine_$WERCKER_APPENGINE_DEPLOY_PATH_LATEST_SDKVERSION.zip
     export PATH="$PATH:$(pwd)/google_appengine"
 fi
-
-
 
 echo "$WERCKER_APPENGINE_DEPLOY_PATH_LATEST_PASSWORD" > "$WERCKER_STEP_TEMP/password"
 
@@ -29,7 +27,8 @@ cd $WERCKER_SOURCE_DIR
 echo "Current pwd"
 pwd
 ls -l
-echo $WERCKER_APPENGINE_DEPLOY_PATH_LATEST_SRCPATH
+echo "SRCPATH: $WERCKER_APPENGINE_DEPLOY_PATH_LATEST_SRCPATH"
+echo "Source_dir: $WERCKER_SOURCE_DIR"
 debug 'Starting deployment of directory'
 appcfg.py update $WERCKER_SOURCE_DIR --email="$WERCKER_APPENGINE_DEPLOY_PATH_LATEST_EMAIL" --passin < "$WERCKER_STEP_TEMP/password"
 
